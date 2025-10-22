1f96ea48cf84eabccd4cda8238954e9b25f932e4
Derek Yuan <87529592+derekyuan1000@users.noreply.github.com>
2025-10-22 20:26:41 +0100

    fix vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-const path = require('path');
+import path from "path";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -12,4 +12,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
